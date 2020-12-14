a108543fcc448fe27cc053bdb7707c7ecdd6ce42
Gregory Savitsky <gregsavvy@pop-os.localdomain>
2020-12-14 21:15:02 +0300

    Infinite scrolling without API pagination

--- src/components/goods-list.component.js
+++ src/components/goods-list.component.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Card from '../../node_modules/react-bootstrap/Card';
 import CardDeck from '../../node_modules/react-bootstrap/CardDeck';
 import Button from '../../node_modules/react-bootstrap/Button';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const context = require.context('../img/', true);
+var i=10;
+var j=20;
 
 const images = {};
 context.keys().forEach((key) => {
@@ -14,8 +16,8 @@ context.keys().forEach((key) => {
 });
 
 const Good = props => (
-    <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" style={{height:150, width:150}} src={images[props.good.img].default} alt="..." />
+    <Card style={{minWidth: '40%', minHeight: '10%'}}>
+    <Card.Img variant="top" style={{maxHeight:'60%', maxWidth:'40%'}} src={images[props.good.img].default} alt="..." />
     <Card.Body>
       <Card.Title>{props.good.param1}</Card.Title>
       <Card.Text>
@@ -30,32 +32,63 @@ export default class GoodsList extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {goods: []};
+    this.state = {goods: [],
+    items: []};
+  }
+
+
+
+  fetchMoreData = () => {
+    function slicearrincrement(arr, i, j) {
+      return arr.slice(i,j)
+      i=i+10
+      j=j+10
+    };
+    // a fake async api call like which sends
+    // 20 more records in 1.5 secs
+    setTimeout(() => {
+      this.setState({
+        items: this.state.items.concat(slicearrincrement(this.state.goods,i,j))
+      });
+    }, 1500);
   }
 
   componentDidMount() {
     axios.get('http://localhost:5000/goods/')
       .then(response => {
-        this.setState({ goods: response.data })
+        this.setState({
+        goods: response.data,
+        items: response.data.slice(0,10)});
       })
       .catch((error) => {
         console.log(error);
       })
-  }
+    }
 
-  goodListTable() {
-    return this.state.goods.map(currentgood => {
+    goodListTable() {
+    return this.state.items.map(currentgood => {
       return <Good good={currentgood} key={currentgood._id}/>;
-    })
+    });
   }
 
   render() {
     return (
       <div>
         <h3>Goods List</h3>
-        <CardDeck>
-        { this.goodListTable() }
-        </CardDeck>
+          <InfiniteScroll
+                dataLength={this.state.goods.length}
+                next={this.fetchMoreData}
+                hasMore={true}
+                loader={<h4>Loading...</h4>}
+            >
+            <div>
+                <CardDeck>
+                { this.goodListTable() }
+                </CardDeck>
+            </div>
+          </InfiniteScroll>
+          {console.log(this.state.goods)}
+          {console.log(this.state.items)}
       </div>
     )
   }
